bd09412f2cb14e580680c9c1b303c7673a19dead
Agustin Chiarotto <agusgc_99@hotmail.com>
2024-04-28 21:50:53 -0300

    correcciones

--- src/Components/Items/Item/Item.js
+++ src/Components/Items/Item/Item.js
@@ -1,56 +1,58 @@
-import './styles.css';
-import Button from '../../Button/Button.js'
+import "./styles.css";
+import Button from "../../Button/button.js";
 
 function Item({ item, index, borrarItem, completarItem }) {
-
-  let nombre = item.nombre
-  let estado = item.estado
-
-
+  let nombre = item.nombre;
+  let estado = item.estado;
 
   if (estado === "completado") {
     return (
-
-
       <div className="Item card border-success mb-3 bg-transparent d-flex flex-column justify-content-center align-items-center position-relative">
-  <div className="card-body text-success">
-    <p className="card-text nombreTarea">{nombre}</p>
-  </div>
+        <div className="card-body text-success">
+          <p className="card-text nombreTarea">{nombre}</p>
+        </div>
 
         <div className="d-flex align-items-start botones">
-          <Button color="danger" type="button" texto="Eliminar" onClick={() => borrarItem(index)} className="mt-1" />
+          <Button
+            color="danger"
+            type="button"
+            texto="Eliminar"
+            onClick={() => borrarItem(index)}
+            className="mt-1"
+          />
         </div>
       </div>
-
-    )
+    );
   } else if (estado === "no completado") {
     return (
-<div className="Item card border-danger mb-3 bg-transparent d-flex flex-column justify-content-center align-items-center position-relative">
-  <div className="card-body text-danger">
-    <p className="card-text nombreTarea">{nombre}</p>
-  </div>
+      <div className="Item card border-danger mb-3 bg-transparent d-flex flex-column justify-content-center align-items-center position-relative">
+        <div className="card-body text-danger">
+          <p className="card-text nombreTarea">{nombre}</p>
+        </div>
         <div className="d-flex align-items-end botones">
-        <div className="me-2">
-      <Button className="completar" color="success" texto="Completar" onClick={() => completarItem(index)} />
-    </div>
-    <div className="ms-auto">
-      <Button color="danger" type="button" texto="Eliminar" onClick={() => borrarItem(index)} className="me-2 mt-1" />
-    </div>
-
+          <div className="me-2">
+            <Button
+              className="completar"
+              color="success"
+              texto="Completar"
+              onClick={() => completarItem(index)}
+            />
+          </div>
+          <div className="ms-auto">
+            <Button
+              color="danger"
+              type="button"
+              texto="Eliminar"
+              onClick={() => borrarItem(index)}
+              className="me-2 mt-1"
+            />
+          </div>
         </div>
       </div>
-
-    )
+    );
   } else {
-    return "error"
+    return "error";
   }
-
-
-
-
-
 }
 //{estado === "no completado" && <Buton ....>}
 export default Item;
-
-
--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,37 +1,47 @@
-import './styles.css';
-import Title from '../../Components/Title/Title.js';
-import AddItem from '../../Components/AddItem/AddItem.js';
-import Items from '../../Components/Items/Items.js'
-import Search from '../../Components/Search/Search.js'
-import {useState} from 'react'
+import "./styles.css";
+import Title from "../../Components/Title/title.js";
+import AddItem from "../../Components/AddItem/AddItem.js";
+import Items from "../../Components/Items/Items.js";
+import Search from "../../Components/Search/Search.js";
+import { useState } from "react";
+
+//Observaciones
+
+//El codigo entregado no compila. Tuve que hacer cambios en los nombres de los import para poder ejecutar la app.
+//Falta mostrar un mensaje cuando ya estan todas las tareas completadas.
+//Title deberia recibir un texto a renderizar por props.
+//El campo 'estado' en tarea deberia ser boolean!
+//Hay archivos styles.css que estan vacios, no deberian existir.
+//Borren siempre los console.logs() antes de hacer una entrega.
+//Dentro de item tienen bastante codigo repetiro, deberian haber hecho un componente ahi para reutilizar ese codigo y no tenerlo dos veces.
+//Los componentes y pages deberian tener el nombre de su archivo comenzando con mayuscula.
+//Nombren los archivos que retornan un jxs, como home y los componentes como .jsx asi se entiende que son elementos de react.
+//Intenten usar styles modules como vimos en clase para evitar tener problemas cuando la app crezca. Para esto nombre los archivos css de cada componente como nombreComponente.module.css
+//Luego importa `import styles from './nombreComponente.module.css' y utilizan el objeto styles para incorporar el css a los className
+//Podrian agregar algunas imagenes de como se ve la aplicacion corriendo al readme?
+//Intenten instalar prettier o algun identador automatico para que el codigo quede mas ordenado y facil de leer.
 
 function Home() {
-
-  const [items, setItems] = useState([])
-  const [searchResults, setSearchResults] = useState(items)
+  const [items, setItems] = useState([]);
+  const [searchResults, setSearchResults] = useState(items);
 
   return (
     <div className="Home">
+      <div className="tituloContainer">
+        <Title />
+      </div>
 
-        <div className='tituloContainer'>
-        <Title/>
+      <div className="row">
+        <div className="col">
+          <Search items={items} setSearchResults={setSearchResults} />
         </div>
-
-        <div className="row">
-    <div className="col">
-      <Search items={items} setSearchResults={setSearchResults} />
-    </div>
-    <div className="col">
-      <AddItem items={items} setItems={setItems} />
-    </div>
-  </div>
-        <div className='itemsContainer'>
-
-       
+        <div className="col">
+          <AddItem items={items} setItems={setItems} />
+        </div>
+      </div>
+      <div className="itemsContainer">
         <Items items={searchResults} setItems={setItems} />
-
-        </div>
-
+      </div>
     </div>
   );
 }
